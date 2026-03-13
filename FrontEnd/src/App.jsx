b1f9649fe9974d1ac257c0f2ac0674b0b7ed2fef
C-Au <aucolin@hotmail.com>
2026-03-13 15:03:41 -0700

    Add routing and home page with product listings
    - Integrated react-router-dom for navigation
    - Created Home component to display a list of products
    - Updated package.json and package-lock.json to include react-router-dom

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <p>HOME PAGE</p>
-    <div/>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/*
+FRONT END:
++ Home page that shows all products in a nice order
+
++ Specific product page to show a product
+
++ Sellers page to view their products
+
++ Sellers page to create and edit products
+
+Checkout page to buy stuff
+  - Review Cart
+  - Enter shipping info
+  - Enter payment detials with stripe
+
+Backend:
+Way to store products
+Way to create products
+Way to Edit products
+Delete products
+
+Get all products
+Get a specific product
+
+Auth (later)
+
+{
+OrderId: 234324324
+OrderName: "Joseph Abate"
+OrderDate; Today
+Items: [item1, item2, item3]
+OrderOwnerId: 3248348
+}
+
+GET MY ORDERS:
+GetAll Reocrds where OrderOwnerId = CurrentSessionUser
+
+*/
